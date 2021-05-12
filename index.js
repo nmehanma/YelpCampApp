e040d23c72c63bf2728a529c0e94b1ddb5463f2b
nmehanma <nmehanma@gmail.com>
2021-05-12 02:07:32 -0400

    Campground Edit & Update functionality implemented

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
+const methodOverride = require('method-override');
 const Campground = require('./models/campground');
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp-app', {
   useNewUrlParser: true,
   useCreateIndex: true,
-  useUnifiedTopology: true
+  useUnifiedTopology: true,
+  useFindAndModify: false
 
 });
 
@@ -27,6 +29,7 @@ app.set('views', path.join(__dirname, 'views'))
 //to parse the body
 
 app.use(express.urlencoded({ extended: true}));
+app.use(methodOverride('_method'))
 
 
 
@@ -44,16 +47,30 @@ app.get('/campgrounds/new', (req,res) => {
   res.render('campgrounds/new')
 })
 
-app.get('/campgrounds/:id', async(req, res,) => {
-  const campground = await Campground.findById(req.params.id)
-  res.render('campgrounds/show', { campground });
-});
-
 app.post('/campgrounds', async(req,res) => {
   const campground = new Campground(req.body.campground);;
   await campground.save();
   res.redirect(`/campgrounds/${campground._id}`)
 });
+
+app.get('/campgrounds/:id', async(req, res,) => {
+  const campground = await Campground.findById(req.params.id)
+  res.render('campgrounds/show', { campground });
+});
+
+app.get('/campgrounds/:id/edit',async(req, res) => {
+  const campground = await Campground.findById(req.params.id)
+  res.render('campgrounds/edit', { campground });
+});
+
+app.put('/campgrounds/:id', async(req, res) => {
+  const { id } = req.params;
+  const campground = await Campground.findByIdAndUpdate(id,{...req.body.campground});
+  res.redirect(`/campgrounds/${campground._id}`)
+})
+
+
+
 
 
 
