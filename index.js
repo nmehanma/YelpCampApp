ad1a6f2005be7507838ee1bde47aef7f05cdcaa2
nmehanma <nmehanma@gmail.com>
2021-05-12 01:46:57 -0400

    Created New & Create functionality

--- index.js
+++ index.js
@@ -24,6 +24,10 @@ const app = express();
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'))
 
+//to parse the body
+
+app.use(express.urlencoded({ extended: true}));
+
 
 
 
@@ -36,10 +40,21 @@ app.get('/campgrounds', async (req, res) => {
   res.render('campgrounds/index', {campgrounds})
 });
 
+app.get('/campgrounds/new', (req,res) => {
+  res.render('campgrounds/new')
+})
+
 app.get('/campgrounds/:id', async(req, res,) => {
   const campground = await Campground.findById(req.params.id)
   res.render('campgrounds/show', { campground });
 });
+
+app.post('/campgrounds', async(req,res) => {
+  const campground = new Campground(req.body.campground);;
+  await campground.save();
+  res.redirect(`/campgrounds/${campground._id}`)
+});
+
 
 
 
