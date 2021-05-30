fba8314dab031330c3d3f62c42c612e7dfd056d8
nmehanma <nmehanma@gmail.com>
2021-05-30 15:23:41 -0400

    Continued updating Flash Errors Partial for success and danger to be rendered dependant on if campground :id is present or not in mongodb

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -41,6 +41,10 @@ router.post('/', validateCampground, catchAsync(async (req, res, next) => {
 
 router.get('/:id', async (req, res, ) => {
   const campground = await Campground.findById(req.params.id).populate('reviews');
+  if (!campground) {
+    req.flash('error', 'Cannot find that campground!');
+    return res.redirect('/campgrounds');
+  }
   res.render('campgrounds/show', { campground });
 });
 
@@ -48,6 +52,10 @@ router.get('/:id', async (req, res, ) => {
 
 router.get('/:id/edit', catchAsync(async (req, res) => {
   const campground = await Campground.findById(req.params.id)
+  if (!campground) {
+    req.flash('error', 'Cannot find that campground!');
+    return res.redirect('/campgrounds');
+  }
   res.render('campgrounds/edit', { campground });
 })
 );
