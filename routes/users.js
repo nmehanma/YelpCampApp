1af19bc03d5d98b8c17d7e2eead03e48cefe7572
nmehanma <nmehanma@gmail.com>
2021-05-31 23:03:00 -0400

    Added a Logout Route and also updated the navbar so that Login/Register/Logout all are ms-auto to the right of the navbar

--- routes/users.js
+++ routes/users.js
@@ -22,14 +22,27 @@ router.post('/register', catchAsync(async (req, res) => {
   }
 }));
 
+//login routes
+
 router.get('/login', (req, res) => {
   res.render('users/login');
 
 });
 
+
 router.post('/login', passport.authenticate('local', { failureFlash: true, failureRedirect: '/login' }), (req, res) => {
   req.flash('success', 'welcome back!');
   res.redirect('/campgrounds');
+});
+
+//logout routes
+
+router.get('/logout', (req, res) => {
+
+  req.logout();
+  req.flash('success', "Goodbye!")
+  res.redirect('/campgrounds')
+
 });
 
 module.exports = router;
