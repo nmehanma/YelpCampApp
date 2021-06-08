28de2ea4ce77e942ac045a8ff54f50a307945c6f
nmehanma <nmehanma@gmail.com>
2021-06-07 23:38:33 -0400

    Continued refactoring with the addition of a reviews and users controller

--- routes/reviews.js
+++ routes/reviews.js
@@ -2,33 +2,15 @@ const express = require('express');
 //set merge params to true so that params is recognized from the index.js file
 const router = express.Router({ mergeParams: true });
 const catchAsync = require('../utils/catchAsync');
-const Campground = require('../models/campground');
-const Review = require('../models/review')
+const reviews = require('../controllers/reviews');
 const { validateReview, isLoggedIn, isReviewAuthor } = require('../middleware')
 
 //creating  new review
 
-router.post('/', isLoggedIn, validateReview, catchAsync(async (req, res) => {
-  const campground = await Campground.findById(req.params.id);
-  const review = new Review(req.body.review);
-  review.author = req.user._id;
-  campground.reviews.push(review);
-  await review.save();
-  await campground.save();
-  req.flash('success', 'Created a new review!')
-  res.redirect(`/campgrounds/${campground._id}`);
-
-}))
+router.post('/', isLoggedIn, validateReview, catchAsync(reviews.createReview))
 
 //reviewId being used beccause we want to remove the reference to the review in the campground and we want to remove the review itself 
-router.delete('/:reviewId', isLoggedIn, isReviewAuthor, catchAsync(async (req, res) => {
-  const { id, reviewId } = req.params;
-  await Campground.findByIdAndUpdate(id, { $pull: { reviews: reviewId } })
-  await Review.findByIdAndDelete(reviewId);
-  req.flash('success', 'Successfully deleted review!')
-  res.redirect(`/campgrounds/${id}`);
-
-}))
+router.delete('/:reviewId', isLoggedIn, isReviewAuthor, catchAsync(reviews.deleteReview));
 
 
 module.exports = router;
