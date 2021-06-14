571790903492441ba867ed51313b18c62c5a0b75
nmehanma <nmehanma@gmail.com>
2021-06-13 20:13:31 -0400

    Fixing our Seed Files

--- seeds/index.js
+++ seeds/index.js
@@ -27,14 +27,25 @@ const sample = array => array[Math.floor(Math.random() * array.length)];
 // random1000 because there are 1000 cities in the array being reviewed, 
 const seedDB = async () => {
   await Campground.deleteMany({});
-  for(let i = 0; i < 50; i++){
+  for (let i = 0; i < 50; i++) {
     const random1000 = Math.floor(Math.random() * 1000);
     const price = Math.floor(Math.random() * 20) + 10;
     const camp = new Campground({
       author: '60b5a64e60ce095bdb868a93',
-      location:`${cities[random1000].city}, ${cities[random1000].state}`,
+      location: `${cities[random1000].city}, ${cities[random1000].state}`,
       title: `${sample(descriptors)} ${sample(places)}`,
-      image: 'https://source.unsplash.com/collection/483251',
+      images: [
+        {
+          url:
+            'https://res.cloudinary.com/doqlk4ocr/image/upload/v1623629024/YelpCamp/hpbx5jusc7msbp3vxsbu.jpg',
+          filename: 'YelpCamp/hpbx5jusc7msbp3vxsbu'
+        },
+        {
+          url:
+            'https://res.cloudinary.com/doqlk4ocr/image/upload/v1623629024/YelpCamp/atzod9rene7r12frxlb2.jpg',
+          filename: 'YelpCamp/atzod9rene7r12frxlb2'
+        }
+      ],
       description: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iste eaque laboriosam fugit cum sequi beatae itaque molestiae aliquid illo, ex voluptate vel assumenda corporis rem ab ipsum odit earum quidem?',
       price
 
