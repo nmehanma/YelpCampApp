d4fe4e3c6aa336c7a87531a81b021926018b42ec
nmehanma <nmehanma@gmail.com>
2021-07-07 21:33:26 -0400

    Clustering Campgrounds

--- public/javascripts/clusterMap.js
+++ public/javascripts/clusterMap.js
@@ -7,6 +7,7 @@ var map = new mapboxgl.Map({
 });
 
 map.on('load', function () {
+  // console.log("Map Loaded!!!")
   // Add a new source from our GeoJSON data and
   // set the 'cluster' option to true. GL-JS will
   // add the point_count property to your source data.
@@ -14,7 +15,7 @@ map.on('load', function () {
     type: 'geojson',
     // Point to GeoJSON data. This example visualizes all M1.0+ earthquakes
     // from 12/22/15 to 1/21/16 as logged by USGS' Earthquake hazards program.
-    data: 'https://docs.mapbox.com/mapbox-gl-js/assets/earthquakes.geojson',
+    data: campgrounds,
     cluster: true,
     clusterMaxZoom: 14, // Max zoom to cluster points on
     clusterRadius: 50 // Radius of each cluster when clustering points (defaults to 50)
@@ -101,6 +102,7 @@ map.on('load', function () {
   // the location of the feature, with
   // description HTML from its properties.
   map.on('click', 'unclustered-point', function (e) {
+    // console.log("UNCLUSTERED POINT CLICKED!")
     var coordinates = e.features[0].geometry.coordinates.slice();
     var mag = e.features[0].properties.mag;
     var tsunami;
@@ -127,6 +129,7 @@ map.on('load', function () {
   });
 
   map.on('mouseenter', 'clusters', function () {
+    // console.log('Mousing Over a Cluster!!!')
     map.getCanvas().style.cursor = 'pointer';
   });
   map.on('mouseleave', 'clusters', function () {
