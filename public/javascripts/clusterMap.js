0dc140efd331a6bf0174cace909837f84de4a4eb
nmehanma <nmehanma@gmail.com>
2021-07-07 22:30:53 -0400

    Adding Custom Popups functionality

--- public/javascripts/clusterMap.js
+++ public/javascripts/clusterMap.js
@@ -103,15 +103,8 @@ map.on('load', function () {
   // description HTML from its properties.
   map.on('click', 'unclustered-point', function (e) {
     // console.log("UNCLUSTERED POINT CLICKED!")
+    const { popUpMarkup } = e.features[0].properties;
     var coordinates = e.features[0].geometry.coordinates.slice();
-    var mag = e.features[0].properties.mag;
-    var tsunami;
-
-    if (e.features[0].properties.tsunami === 1) {
-      tsunami = 'yes';
-    } else {
-      tsunami = 'no';
-    }
 
     // Ensure that if the map is zoomed out such that
     // multiple copies of the feature are visible, the
@@ -122,10 +115,9 @@ map.on('load', function () {
 
     new mapboxgl.Popup()
       .setLngLat(coordinates)
-      .setHTML(
-        'magnitude: ' + mag + '<br>Was there a tsunami?: ' + tsunami
-      )
+      .setHTML(popUpMarkup)
       .addTo(map);
+
   });
 
   map.on('mouseenter', 'clusters', function () {
